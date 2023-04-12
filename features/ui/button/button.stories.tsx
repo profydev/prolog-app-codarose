a8238ee5d6870c1ecd8948de6b3c62234be30120
Esther Rose <estherrose046@gmail.com>
2023-04-12 13:04:15 -0500

    updated so that there is one button component

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,30 +1,29 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import action from "@storybook/addon-actions";
-import { StyledButton, ButtonVariant, ButtonSize } from "./styled-button";
+import { Button, ButtonVariant, ButtonSize } from "./button";
 
 export default {
   title: "UI/Button",
-  component: StyledButton,
+  component: Button,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
   },
   argTypes: { onClick: { action: "clicked" } },
-} as ComponentMeta<typeof StyledButton>;
+} as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof StyledButton> = ({ size, variant }) => (
+const Template: ComponentStory<typeof Button> = ({ size, variant }) => (
   <div style={{ padding: 50 }}>
-    <StyledButton variant={variant} size={size}>
+    <Button variant={variant} size={size}>
       Label
-    </StyledButton>
+    </Button>
   </div>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  size: ButtonSize.sm,
-  variant: ButtonVariant.primary,
+  size: ButtonSize.lg,
+  variant: ButtonVariant.inherit,
 };
 Default.parameters = {
   viewMode: "docs",
